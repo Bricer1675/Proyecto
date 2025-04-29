c3b1fcda54501e0eca4886f3bbe545e9e83c1302
MasterCharles <charrles.ia@gmail.com>
2025-04-28 20:10:14 -0500

    actualuzacion de esto

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,23 +5,48 @@ function agregarAlCarrito(nombre, precio) {
   localStorage.setItem("carrito", JSON.stringify(carrito));
   alert("Producto agregado al carrito.");
   actualizarCantidadCarrito();
-
-  // Animación de botón
-  const botonCarrito = document.getElementById("boton-carrito");
-  if (botonCarrito) {
-    botonCarrito.classList.add("rebote");
-    setTimeout(() => botonCarrito.classList.remove("rebote"), 600);
-  }
 }
 
 // Actualizar cantidad de productos en el carrito
 function actualizarCantidadCarrito() {
   const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-  const botonCarrito = document.getElementById("boton-carrito");
-  if (botonCarrito) {
-      botonCarrito.innerText = carrito.length;
+  const cantidad = document.getElementById("cantidad-carrito");
+  if (cantidad) {
+    cantidad.innerText = carrito.length;
   }
 }
 
+// Mostrar los productos en carrito.html
+function mostrarCarrito() {
+  const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  const contenedor = document.getElementById("lista-productos");
+  const total = document.getElementById("total-carrito");
+  let suma = 0;
+
+  if (contenedor) {
+    contenedor.innerHTML = ""; // limpiar
+    carrito.forEach(producto => {
+      contenedor.innerHTML += `<p>${producto.nombre} - $${producto.precio}</p>`;
+      suma += producto.precio;
+    });
+
+    if (total) {
+      total.innerText = `$${suma}`;
+    }
+  }
+}
+
+// Vaciar el carrito
+function vaciarCarrito() {
+  localStorage.removeItem("carrito");
+  mostrarCarrito();
+  actualizarCantidadCarrito();
+}
+
 // Ejecutarlo cuando cargue la página
-document.addEventListener('DOMContentLoaded', actualizarCantidadCarrito);
+document.addEventListener('DOMContentLoaded', () => {
+  actualizarCantidadCarrito();
+  if (document.getElementById("lista-productos")) {
+    mostrarCarrito();
+  }
+});
